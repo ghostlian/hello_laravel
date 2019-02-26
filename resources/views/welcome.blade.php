ca14c85061edb60cbf6a1f6e409c0ed08dbe2a29
lianqq <786002241@qq.com>
2019-02-26 15:53:07 +0000

    alert welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-lqq
+                    Laravel-lqq-woaini
                 </div>
 
                 <div class="links">
